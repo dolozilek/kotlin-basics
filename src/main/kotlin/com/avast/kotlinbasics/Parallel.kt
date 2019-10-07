591568fa0c6580c97a407fe2fa74c6770179284c
dolozilek <martin.dolozilek@avast.com>
2019-10-07 20:19:10 +0200

    coroutines

--- src/main/kotlin/com/avast/kotlinbasics/Parallel.kt
+++ src/main/kotlin/com/avast/kotlinbasics/Parallel.kt
@@ -1,5 +1,8 @@
 package com.avast.kotlinbasics
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import reactor.core.publisher.Flux
 import reactor.core.publisher.toFlux
@@ -10,8 +13,8 @@ object Operation {
     private val logger = LoggerFactory.getLogger(Operation::class.java)
 
 
-    fun run(value: String):Boolean {
-//        Thread.sleep(1000)
+    fun run(value: String): Boolean {
+        Thread.sleep(1000)
         logger.info("$value done")
         return true
     }
@@ -26,7 +29,6 @@ object Sequential {
 //        return data.map { pattern.matcher(it).matches() }
         return data.map(Operation::run)
 //                .doOnNext { logger.info("Processing $it") }
-//                .map { pattern.matcher(it).matches() }
     }
 }
 
@@ -36,7 +38,7 @@ object Parallel {
     private val logger = LoggerFactory.getLogger(Parallel::class.java)
 
     fun run(data: Flux<String>): Flux<Boolean> {
-        return data.parallel(5).runOn(Schedulers.elastic())
+        return data.parallel(20).runOn(Schedulers.elastic())
                 .map(Operation::run).sequential()
 //                .doOnNext { logger.info("Processing $it") }
 //                .map { pattern.matcher(it).matches() }.sequential()
@@ -58,6 +60,28 @@ fun main() {
     println("Sequential: ${seqEnd - seqStart} ms")
     println("Parallel: ${parEnd - parStart} ms")
 
+    val susStart = System.currentTimeMillis()
+
+    runBlocking {
+        println(Suspend.run(data).size)
+    }
+    val susEnd = System.currentTimeMillis()
+    println("Suspend: ${susEnd - susStart} ms")
 }
 
+object Suspend {
+    private val logger = LoggerFactory.getLogger(Suspend::class.java)
+    private val pattern = Pattern.compile(".*")
 
+    suspend fun run(data: List<String>): List<Boolean> {
+
+        return data.map {
+            GlobalScope.async {
+
+//                logger.info("Processing $it")
+//                pattern.matcher(it).matches()
+                Operation.run(it)
+            }
+        }.map { it.await() }
+    }
+}
